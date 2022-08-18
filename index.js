709eb464c991b6e7953661812e8a36fdbea681fa
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-18 22:38:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+ // Write your code here!
+ if(ride<="400"){
+  return 'This one is on me!'
+}
+else if(ride >2000 && ride <=2500)
+{
+
+  return 'I will gladly take your thirty bucks.'
+}
+else if(ride > 2500) {
+  return 'No can do.'
+}
+}
+  
+
+
   // Write your code here!
+  function ternaryCheckCity(city){
+    return city==='NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous': return 'Thank you so much.'
+    break;
+    case 'not as generous': return 'Thank you.'
+    break;
+    default: return 'Bye.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
